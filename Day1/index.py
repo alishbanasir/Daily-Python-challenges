dd810dee8f3f99a6c6a4dbee26bea7df4f295754
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-02-25 16:27:06 +0500

    first commit

---
+++ Day1/index.py
@@ -0,0 +1,4 @@
+#💻 Task 1: Print a Message 
+#🚀 Challenge: Write a Python program that prints the following message on the screen:
+
+print("Hello, World! Welcome to Python Programming. Let's start coding!")
