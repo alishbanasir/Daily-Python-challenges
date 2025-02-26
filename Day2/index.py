74463e403736e3ca6d20e8f93d8263e5b02e41ef
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-02-26 19:48:16 +0500

    code

--- Day2/index.py
+++ Day2/index.py
@@ -1,3 +1,9 @@
+# Task 2
+'''Ask the user for their name using input(). 
+Display a cool welcome message like a high-tech AI would! 🚀
+Make the message fun and interactive using string formatting!
+'''
+
 user_input=input("Enter your name: ")
 print(f"Processing... Name detected: {user_input}" )
 print(f"Access Granted! Welcome, Agent {user_input}. Your mission awaits! ")
