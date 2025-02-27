82ff2778b51f4c675b42ab4579e15c32828b140e
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2025-02-27 21:16:35 +0500

    code

---
+++ Day3/index.py
@@ -0,0 +1,32 @@
+#Task 3
+print("----------------------- Addition ----------------------------")
+user_1st_number=int(input("Enter First Number: "))
+user_2nd_number=int(input("Enter Second Number: "))
+print(f"{user_1st_number} + {user_1st_number} =",user_1st_number + user_2nd_number)
+
+print("\n")
+print("----------------------- Subtraction -------------------------")
+user_1st_number=int(input("Enter First Number: "))
+user_2nd_number=int(input("Enter Second Number: "))
+print(f"{user_1st_number} - {user_1st_number} =",user_1st_number - user_2nd_number)
+
+print("\n")
+print("---------------------- Multiplication -----------------------")
+user_1st_number=int(input("Enter First Number: "))
+user_2nd_number=int(input("Enter Second Number: "))
+print(f"{user_1st_number} * {user_1st_number} =",user_1st_number * user_2nd_number)
+
+print("\n")
+print("----------------------- Division ----------------------------")
+user_1st_number=int(input("Enter First Number: "))
+user_2nd_number=int(input("Enter Second Number: "))
+print(f"{user_1st_number} / {user_1st_number} =",user_1st_number / user_2nd_number)
+
+
+
+
+
+
+
+
+
